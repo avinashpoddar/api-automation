f07479d1a0f1ac873be59e82e80b6c7c79bcce4a
Avinash Poddar <avinashpoddar10@gmail.com>
2019-12-21 17:32:50 +0530

    added lombok and refactored testng gradle compile path

--- src/main/java/entities/reqres.in/UserResponse.java
+++ src/main/java/entities/reqres.in/UserResponse.java
@@ -1,50 +1,25 @@
 package entities.reqres.in;
-import entities.base.BaseResponse;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import entities.base.BaseResponse;
+import lombok.Getter;
+import lombok.Setter;
 
-@JsonIgnoreProperties(
-        ignoreUnknown = true
-)
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserResponse extends BaseResponse {
+    private Data data;
 
-    Data data;
-
-    public  UserResponse(){}
-
-    // Getter Methods
-    public Data getData() {
-        return data;
-    }
-
+    @Getter
+    @Setter
+    @JsonIgnoreProperties(ignore Unknown = true)
     public static class Data {
         private float id;
         private String email;
         private String first_name;
         private String last_name;
         private String avatar;
-
-        // Getter Methods
-
-        public float getId() {
-            return id;
-        }
-
-        public String getEmail() {
-            return email;
-        }
-
-        public String getFirst_name() {
-            return first_name;
-        }
-
-        public String getLast_name() {
-            return last_name;
-        }
-
-        public String getAvatar() {
-            return avatar;
-        }
     }
-
 }
 
